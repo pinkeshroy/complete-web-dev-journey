e3b33030d7a3f90d19699ff3ec26eb645e9c56bb
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-08-17 13:39:39 +0530

    question

---
+++ questions.js
@@ -0,0 +1,13 @@
+//Question 07
+// let map = new Map()
+// map.set("Alice", 85)
+// map.set("Bob", 92)
+// map.set("Charlie", 78)
+// console.log(`Bob marks is: ${map.get("Bob")}`);
+
+// Question: You are building a chat application. Create a Map called userMessages where keys are user objects and values are arrays of messages sent by each user. Implement a function sendMessage(user, message) that adds a new message to the user’s array of messages. Also, implement a function getTotalMessages(user) that returns the total number of messages sent by a user.
+let userMessages = new Map()
+
+// function sendMessage(user, message) {
+//     userMessages.set(user,[].push(message))
+// }
