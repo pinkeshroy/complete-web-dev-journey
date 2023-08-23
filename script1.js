2c259a165e7c2539721266eed60f527fb21934b2
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-08-23 11:16:15 +0530

    formValidation

--- script1.js
+++ script1.js
@@ -1,14 +1,12 @@
-let name = document.getElementById('name')
-let age = document.getElementById('age')
-let email = document.getElementById('email')
-let male = document.getElementById('male')
-let female = document.getElementById('female')
-let phone=document.getElementById('phone')
-let errorMessage = document.getElementsByTagName('p')[0];
-console.log(errorMessage)
+const name = document.getElementById('name')
+const age = document.getElementById('age')
+const email = document.getElementById('email')
+const male = document.getElementById('male')
+const female = document.getElementById('female')
+const phone=document.getElementById('phone')
+const errorMessage = document.getElementById('error');
 
 function validateInput(input) {
-    console.log(input)
     if (input === "age") {
         if (age.value < 18) {
             errorMessage.innerText = "Age must be greater than 18"
@@ -21,25 +19,16 @@ function validateInput(input) {
         }
     }
     else if (input === "phone") {
-        if (phone.value.length < 10)
+        if (phone.value.length !== 10) {
             errorMessage.innerText = "phone must be 10 digit"
-        else if (phone.value.length > 10)
-            errorMessage.innerText = "phone must be 10 digit"
-        else {
-            errorMessage.innerText = ""
         }
-    }
-    else if (input === "name") {
-        if (name.value.length < 10)
-            errorMessage.innerText = "name must be 5 of 10 character"
         else {
             errorMessage.innerText = ""
         }
     }
 }
-const handleForm = (event) =>{
+const handleSubmitForm = (event) =>{
     event.preventDefault();
-    console.log("event handeled")
     console.log({
         name: name.value,
         age: age.value,
@@ -48,7 +37,7 @@ const handleForm = (event) =>{
     })
 }
 
-let submitted = document.getElementById('btn')
+const submitted = document.getElementById('submitButton')
 submitted.addEventListener('click', (event) => {
     submitted.innerText="Submitted"
 })
