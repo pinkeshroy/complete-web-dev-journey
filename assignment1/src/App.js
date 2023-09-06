f7a5d899cb60a3aead0d66ebb5131f0a00e16480
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-09-06 23:49:09 +0530

    reactFirstAssignment

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -5,35 +5,57 @@ import { useState } from "react";
 function App() {
   const [input, setInput] = useState('')
   const [data, setData] = useState([])
-  const li=document.getElementById('inputValue')
+  const li = document.getElementById('inputValue')
+  const dataList = document.querySelector('.dataList')
+  const cardBox=document.getElementById('cardBox')
   const updatesetInput = (event) => {
     setInput(event.target.value)
-    // console.log(event.target.value)
   }
   const upadatesetData = (event) => {
     const newData = [input, ...data];
     setData(newData)
     setInput('')
-    console.log(newData)
   }
-  const updateSetData = (event)=>{
+  const updateSetData = (event) => {
+    cardBox.style.display = 'none';
+    console.log(cardBox);
     const newData = [...data];
     const id = event.target.id;
-    console.log({id})
     newData.splice(id,1)
     setData(newData);
-    console.log(newData);
+  }
+  const showCardsonUI = (e) => {
+    if (e.target.checked) {
+      cardBox.style.display = 'flex';
+      dataList.style.display = 'none';
+      console.log(e.target.checked);
+    }
+    else {
+      cardBox.style.display = 'none';
+      dataList.style.display = 'block';
+      console.log(cardBox,dataList)
+    }
   }
   return (
     <div className="App">
-      <div><input id='inputValue' value={input} onChange={updatesetInput}></input>
-        <button onClick={upadatesetData}>Submit</button></div>
-      <div>
-        <ol className='dataList'>{
-          data.map((elem, idx) => {
-            return <li>{elem} <span > <a id={idx} onClick={updateSetData} href='#'>X</a></span></li>
-          })
-        }</ol></div>
+      <form onSubmit={(e)=>{e.preventDefault()}}><input id='inputValue' value={input} onChange={updatesetInput}></input>
+        <button onClick={upadatesetData}>Submit</button>
+        <label>Cards </label>
+        <input id='checkBox' type='checkbox' onClick={showCardsonUI} />
+      </form>
+      <div id='cardBox'>{
+        data.map((elem, idx) => {
+          return <div className='card'><p id={idx} onClick={updateSetData}>X</p>
+            <h3>{elem}</h3></div>
+        })
+      }
+      </div>
+
+      <ol className='dataList'>{
+        data.map((elem, idx) => {
+          return <li>{elem}<a id={idx} onClick={updateSetData} href='#'>X</a></li>
+        })
+      }</ol>
     </div>
   );
 }
