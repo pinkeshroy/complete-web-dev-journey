b34abab1d63c0df917c3bb814406a6ef0c67367d
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-09-27 12:35:02 +0530

    2

--- ques2.js
+++ ques2.js
@@ -4,15 +4,13 @@
 // function getLeftSwappedArrayByIndex(arr, swapLeftBy) - this will return an swapped array with given index
 
 function getLeftSwappedArrayByIndex(arr, swapLeftBy) {
-    const newArr=[...arr]
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 0; j < swapLeftBy; j++){
-            const elem = newArr.shift()
-            newArr.push(elem)
-            
-        }
+    let newArr = []
+    for (let i = 0; i < swapLeftBy; i++){
+        newArr.push(arr.shift())
     }
-    console.log(newArr)
+    newArr=[...arr,...newArr]
+    console.log(newArr);
+
 }
 
 const arr = [1, 2, 3, 4, 5, 6];
