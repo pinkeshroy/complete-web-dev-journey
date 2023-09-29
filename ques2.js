3479d3636277111e673156e425b307c796301186
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-09-29 12:18:12 +0530

    2

--- ques2.js
+++ ques2.js
@@ -1,6 +1,6 @@
 // Find the greatest dublicate number among dublicates
 // function findMaxDublicate(arr) - this function will return the max dublicate number of an array
-// const arr = [11,1,12,2,5,5,6,11,14,11]
+// const arr = [11,1,12,12,12,2,5,5,6,11,14,11]
 // output - 11 - 11 is dublicate and the biggest number among all dublicates
 
 function findMaxDublicate(arr) {
@@ -9,7 +9,7 @@ function findMaxDublicate(arr) {
     for (let i = 0; i < arr.length; i++){
         if (map.get(arr[i])) {
             map.set(arr[i], map.get(arr[i]) + 1)
-            if (max[1] < map.get(arr[i])) {
+            if (max[1] <= map.get(arr[i])&& max[0]<=arr[i]) {
                 max[0] = arr[i];
                 max[1] = map.get(arr[i]);
             }
@@ -26,7 +26,7 @@ function findMaxDublicate(arr) {
     return max[0]
     
 }
-const arr = [11, 1, 12, 2, 5, 5, 6, 11, 14, 11];
+const arr = [11, 1, 12,12,12, 2, 5, 5, 6, 11, 14, 11];
 
 console.log(findMaxDublicate(arr))
 
