015a0562a0812f4e08d2261def3b184acca1468f
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-10-12 11:23:42 +0530

    1

---
+++ ques1.js
@@ -0,0 +1,26 @@
+// Write a function to find dublicates elements without using an set and map, do it O(n), do not use nested loops
+// const arr = [1,1,2,3,4,4,4,5,6,6,7]
+// output - [1,4,6]
+// function getDublicates(arr) - this function will return array of dublicates in O(n)
+
+function getDublicates(arr) {
+    const obj = {};
+    for (const elem of arr) {
+        if (obj[elem]) {
+            obj[elem]=obj[elem]+1
+        } else {
+            obj[elem]=1
+        }
+    }
+    const newArr = [];
+    for (const [key,value] of Object.entries(obj)) {
+        if (value > 1) {
+            newArr.push(Number(key))
+        }
+    }
+    return newArr
+    
+}
+const arr = [1, 1, 2, 3, 4, 4, 4, 5, 6, 6, 7];
+
+console.log(getDublicates(arr));
