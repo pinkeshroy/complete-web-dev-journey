11fda08920f1fbc1c0c6d55458a59798703f13b4
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-10-16 11:20:00 +0530

    1

---
+++ ques1.js
@@ -0,0 +1,23 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+// consisting of non-space characters only.
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = " fly me to the moon "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+function countTheStringLength(s="") {
+    const arrS = s.trim().split(" ")
+    const str = arrS[arrS.length - 1]
+    return str.length;
+}
+const s = " fly me to the moon ";
+console.log(countTheStringLength(s));
