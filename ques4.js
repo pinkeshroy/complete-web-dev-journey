9e2ad3820f7f35da847f82978c0e304e35f47d52
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-10-17 11:56:07 +0530

    4

---
+++ ques4.js
@@ -0,0 +1,29 @@
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+// Example 1:
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
+
+function countbinaryConsecutiveOnes(arr=[]) {
+  let tempcount=0, maxCount = 0;
+    
+  for (const elem of arr) {
+    if (elem === 1) {
+      ++tempcount;
+    } else {
+      maxCount = tempcount;
+      tempcount = 0;
+    }
+  }
+  if (tempcount > maxCount)
+    return tempcount;
+  return maxCount;
+}
+const nums = [1, 1, 0, 1, 1, 1];
+console.log(countbinaryConsecutiveOnes(nums));
